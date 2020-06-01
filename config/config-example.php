cba0f302bec6640ecebfef1ce658378f14494846
Atlas <doroshuk33@yandex.by>
2020-06-01 12:41:59 +0300

    config-example

---
+++ config/config-example.php
@@ -0,0 +1,17 @@
+<?php
+
+//Данные для подключения к БД
+define('HOST', '');
+define('DBNAME', '');
+define('USER', '');
+define('PASSWORD', '');
+
+//Время действия куки для авторизации при отсутствии сессии
+define('TIMEOUT_USER_HASH', 50000);
+
+//путь к папке с шаблонами для TWIG шаблонизатора
+define('PATH_TEMPLATES', $_SERVER['DOCUMENT_ROOT'].'/public/templates');
+//Поддерживаемые языки (алиас)
+define ('LANG', ['en', 'ru']);
+//Язык по умолчанию
+define ('DEFAULT_LANG', 'ru');
